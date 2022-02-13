ff339e16ac4eb888cd4e4eea9d3f65b90d791a82
Ali <97373457+DevNull-IR@users.noreply.github.com>
2022-02-13 10:35:30 +0330

    Create select.php

---
+++ Exsample/select.php
@@ -0,0 +1,9 @@
+<?php 
+
+include_once '../src/DB.php';
+
+$him = select('*','db_product',['id'=>"3"],'limit 10');
+
+foreach($him['fetchAll'] as $key=>$value){
+  echo $value['Referee'] . "<br />";
+}
