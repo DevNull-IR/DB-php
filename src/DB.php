79e4c0e26eb57c8bb165f65aa6517b08dd3468c0
Ali <97373457+DevNull-IR@users.noreply.github.com>
2022-02-13 13:56:38 +0330

    Update DB.php

--- src/DB.php
+++ src/DB.php
@@ -78,3 +78,36 @@ function insert(string $table,array $array){
     $result->execute();
     return $result->rowCount();
 }
+
+
+function deleted(string $table ,$where = "None",string $other = null){
+	global $pdo;
+	$a = null;
+      if (isset($other) && !empty($other)){
+      $other = trim($other);
+      $other = " $other";
+  } else {
+      $other = null;
+  }
+    if ($where === "None"){
+      $where_q = "1";
+  } else if (gettype($where) == "array"){
+        foreach($where as $key => $value){
+      $a .= "$key = ? and ";
+      $answer[] = $value;
+        }
+      $where_q = preg_replace("/ and(?=( \w+)?$)/", null, trim($a));
+  } else {
+      $where_q = "1";
+  }
+	$query = "DELETE FROM $table WHERE $where_q $other";
+    $query = trim($query);
+      $result = $pdo->prepare($query);
+  if (gettype($where) == "array"){
+	for($i = 1;$i<count($where) +1; $i++){
+    $result->bindValue($i,$answer[$i -1]);
+    }
+  }
+    $result->execute();
+    return $result->rowCount();
+}
