bacc2c3731f744322467a4be487bd3e02663639b
Ali <97373457+DevNull-IR@users.noreply.github.com>
2022-02-19 11:23:13 +0330

    Update DB.php

--- src/DB.php
+++ src/DB.php
@@ -1,60 +1,63 @@
 <?php
  $pdo = null;
 function connect($dbname,$username_db,$password_db,$host = 'localhost'){
-    global $pdo;
-    $Option = [
-    PDO::ATTR_PERSISTENT => TRUE,
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::MYSQL_ATTR_INIT_COMMAND =>'SET NAMES utf8',
-    PDO::ATTR_EMULATE_PREPARES => false
-];
-
-try {
+        global $pdo;
+        $Option = [
+        PDO::ATTR_PERSISTENT => TRUE,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::MYSQL_ATTR_INIT_COMMAND =>'SET NAMES utf8',
+        PDO::ATTR_EMULATE_PREPARES => false
+    ];
     
-    $pdo = new PDO("mysql:host=". $host .";dbname=". $dbname .";charset=utf8", $username_db , $password_db , $Option ); // set and connect to db by pdo
-    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); // not hack :)
-    
-} catch (PDOException $error) {
-    file_put_contents("ErrorDB.log",$error->getMessage().PHP_EOL,8);
-    die("Error To Connected To Mysql");
-}
+    try {
+        $pdo = new PDO("mysql:host=". $host .";dbname=". $dbname .";charset=utf8", $username_db , $password_db , $Option ); // set and connect to db by pdo
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); // not hack :)
+        
+    } catch (PDOException $error) {
+        file_put_contents("ErrorDB.log",$error->getMessage().PHP_EOL,8);
+        die("Error To Connected To Mysql");
+    }
 
 }
 function select(string $select, string $db,$where = "None",string $other = null){
     global $pdo;
-  $a = null;
-  $answer = [];
-  if (isset($other) && !empty($other)){
-      $other = trim($other);
-      $other = " $other";
-  } else {
-      $other = null;
-  }
-  if ($where === "None"){
-      $where_q = "1";
-  } else if (gettype($where) == "array"){
-        foreach($where as $key => $value){
-      $a .= "$key = ? and ";
-      $answer[] = $value;
-        }
-      $where_q = preg_replace("/ and(?=( \w+)?$)/", null, trim($a));
-  } else {
-      $where_q = "1";
-  }
-  $query = 'select ' . $select . ' from ' . $db . ' where ' . $where_q . $other;
-  $result = $pdo->prepare($query);
-  if (gettype($where) == "array"){
-	for($i = 1;$i<count($where) +1; $i++){
-    $result->bindValue($i,$answer[$i -1]);
+    $a = null;
+    $answer = [];
+    if (isset($other) && !empty($other)){
+        $other = trim($other);
+        $other = " $other";
+    } else {
+        $other = null;
     }
-  }
+    if ($where === "None"){
+        $where_q = "1";
+    } else if (gettype($where) == "array"){
+        foreach($where as $key => $value){
+            $a .= "$key = ? and ";
+            $answer[] = $value;
+        }
+        $where_q = preg_replace("/ and(?=( \w+)?$)/", null, trim($a));
+    } else {
+        $where_q = "1";
+    }
+    $query = 'select ' . $select . ' from ' . $db . ' where ' . $where_q . $other;
+    $result = $pdo->prepare($query);
+    if (gettype($where) == "array"){
+        for($i = 1;$i<count($where) +1; $i++){
+        $result->bindValue($i,$answer[$i -1]);
+        }
+    }
     $result->execute();
-  $execute = [
-    'count' => $result->rowCount(),
-    'fetchAll' => $result->fetchall(PDO::FETCH_ASSOC),
-    #'fetch' => $result->fetch(PDO::FETCH_ASSOC)
-  ];
-  return $execute;
+    $execute = [
+        'count' => $result->rowCount(),
+        'fetchAll' => $result->fetchall(PDO::FETCH_ASSOC),
+        #'fetch' => $result->fetch(PDO::FETCH_ASSOC)
+    ];
+    $execute['fetch'] = $execute['fetchAll'][0] ?? null;
+    if ($execute['fetch'] == null){
+        unset($execute['fetch']);
+    }
+    return $execute;
 }
 
 
