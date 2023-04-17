51b810b731fd3b25d82d360e69008350b73f5fd3
Ali <97373457+DevNull-IR@users.noreply.github.com>
2023-04-17 19:13:08 +0330

    Update select.php

--- Exsample/select.php
+++ Exsample/select.php
@@ -2,8 +2,8 @@
 
 include_once '../src/DB.php';
 
-$him = select('*','db_product',['id'=>"3"],'limit 10');
+$him = select('*' ,'db_product',['id'=>'3'], 'limit 10');
 
-foreach($him['fetchAll'] as $key=>$value){
+foreach ($him['fetchAll'] as $key=>$value) {
   echo $value['Referee'] . "<br />";
 }
